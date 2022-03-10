b4708b35e6182dca27da50dcf7a8091def492337
GloriaBargoria <bargoriagloria@gmail.com>
2022-03-10 09:37:29 +0300

    updates slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -7,7 +7,7 @@ const Container = styled.div`
     height: 100vh;
     width: 100%;
     display: flex;
-    background-color: pink;
+    // background-color: pink;
     position: relative;
     overflow: hidden;
 `
@@ -27,52 +27,60 @@ const Arrow = styled.div`
     right: ${(props)=> props.direction === "right" && "10px"};
     cursor: pointer;
     opacity: 0.5;
+    z-index: 2;
 `
 const Wrapper = styled.div`
     height: 100%;
     display: flex;
+    transform: translateX(0vw);
 `
 const Slide = styled.div`
     width: 100vw;
     height: 100vh;
     display: flex;
     align-items: center;
+    background-color: #${props=>props.bg}
 `
 const ImgContainer = styled.div`
     flex: 1;
     height: 100%;
 `
 const Image = styled.img`
-    height: 80%;
+    height: 100%;
+    width: 50vw;
 `
 const InfoContainer = styled.div`
     flex: 1;
     padding: 50px;
 `
 const Title = styled.h1`
-    font-size: 70px;
+    font-size: 60px;
 `
 const Desc = styled.p`
     marging: 50px 0px;
     font-weight: 500;
     letter-spacing: 3px; 
 `
-const Button = styled.button`
+const Button = styled.button`onClick={()=>handleClick("left")}
     padding: 10px;
     font-size: 20px;
     cursor: pointer;
 `
+//<a href="https://ibb.co/LY93BMC"><img src="https://i.ibb.co/fqrsZbC/woman-gb45a0bae1-1280.png" alt="woman-gb45a0bae1-1280" border="0"></a>
 
 const Slider = () => {
+    const handleClick = (direction) => {
+
+    }
   return (
     <Container>
-        <Arrow direction="left">
+        <Arrow direction="left" onClick={()=>handleClick("left")}>
             <ArrowLeftOutlinedIcon />
         </Arrow>
         <Wrapper>
-            <Slide bg='yellow'>
+            <Slide bg='#33fff9'>
                 <ImgContainer>
-                    <Image src="https://i.ibb.co/XsdmR2c/1.png" alt='image'/>
+                    <Image src="https://i.ibb.co/fqrsZbC/woman-gb45a0bae1-1280.png" alt='image'/>
                 </ImgContainer>
                 <InfoContainer>
                     <Title>SUMMER SALE</Title>
@@ -82,7 +90,7 @@ const Slider = () => {
             </Slide>
             <Slide bg='blue'>
                 <ImgContainer>
-                    <Image src="https://i.ibb.co/XsdmR2c/1.png" alt='image'/>
+                    <Image src="https://i.ibb.co/fqrsZbC/woman-gb45a0bae1-1280.png" alt='image'/>
                 </ImgContainer>
                 <InfoContainer>
                     <Title>WINTER SALE</Title>
@@ -90,8 +98,18 @@ const Slider = () => {
                     <Button>SHOP NOW</Button>
                 </InfoContainer>
             </Slide>
+            <Slide bg='blue'>
+                <ImgContainer>
+                    <Image src="https://i.ibb.co/SmSZxKW/woman-g0a2625ca6-1280.jpg" alt='image'/>
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>POPULAR SALE</Title>
+                    <Desc>DONT COMPROMISE ON STYLE!</Desc>
+                    <Button>SHOP NOW</Button>
+                </InfoContainer>
+            </Slide>
         </Wrapper>
-        <Arrow direction="right">
+        <Arrow direction="right" onClick={()=>handleClick("right")}>
             <ArrowRightOutlinedIcon />
         </Arrow>
     </Container>
