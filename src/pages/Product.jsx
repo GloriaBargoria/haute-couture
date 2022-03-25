14a6ec06e917be90def4685a2b4c573e3d562bf6
GloriaBargoria <bargoriagloria@gmail.com>
2022-03-25 18:04:44 +0300

    products feature updated

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -126,11 +126,7 @@ const Product = () => {
         <InfoContainer>
           <Title>Denim Jumpsuit</Title>
           <Desc>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            venenatis, dolor in finibus malesuada, lectus ipsum porta nunc, at
-            iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
-            tristique tortor pretium ut. Curabitur elit justo, consequat id
-            condimentum ac, volutpat ornare.
+            Beat the cold winter looking stylish in these amazing Denim Jumpsuits. Don't be cold, be bold and upgrade your wardrobe today!
           </Desc>
           <Price>$ 20</Price>
           <FilterContainer>
