ff1c3955b12eb536a17b5413a47f90a57e02c669
GloriaBargoria <bargoriagloria@gmail.com>
2022-03-25 17:46:01 +0300

    styled components for products

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -103,7 +103,16 @@ const Amount = styled.span`
   margin: 0px 5px;
 `;
 
-
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover{
+      background-color: #f8f4f4;
+  }
+`;
 
 const Product = () => {
   return (
