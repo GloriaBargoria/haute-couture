f9293442fad0a05d54f078d59745a8f571ed7907
GloriaBargoria <bargoriagloria@gmail.com>
2022-03-28 15:20:35 +0300

    styled components for product list

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -17,7 +17,24 @@ const FilterContainer = styled.div`
   justify-content: space-between;
 `;
 
+const Filter = styled.div`
+  margin: 20px;
+  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
+`;
 
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+  ${mobile({ marginRight: "0px" })}
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+  ${mobile({ margin: "10px 0px" })}
+`;
+const Option = styled.option``;
 
 const ProductList = () => {
   return (
